910c8e014d688375e9dcc368ba36a211b69b895d
Joyce Tipping <joycetipping@rose.(none)>
2010-06-24 01:30:52 -0600

    Made significant improvement to the plugin. :)

--- jquery.truncate.js
+++ jquery.truncate.js
@@ -2,44 +2,48 @@
   $.fn.extend ({
 
     truncate : function (options) {
-      var settings = $.extend ({
-        length    : 300,
-        more_text : 'more',
-        less_text : 'less'
-      }, options);
+      var settings = $.extend ({}, $.fn.truncate.defaults, options);
 
       return this.each (function () {
         var text = $(this).text ();
-        if (text.length > settings.length) {
-          var split_pt = text.indexOf (' ', settings.length);
-          if (split_pt !== -1) {
-            var teaser = create ('span', text.substring (0, split_pt));
-            var rest   = create ('span', text.substring (split_pt), css ({display:'none'}));
-          }
-        }
+        var height = settings.lines_visible * settings.line_height_px + 2;
 
-        var $more_less_link = $(create ('a', settings.more_text,
-                                             {href: 'javascript:void(0);'},
-                                             css ({padding:'0 5px'}))
-                              ).toggle (function () {
-                                $(this).text (settings.less_text);
-                                $(rest).show ('fast');
-                              },        function () {
-                                $(this).text (settings.more_text);
-                                $(rest).hide ('fast');
-                              });
+        var more_less_link = create ('a', settings.more_text,
+                                          {href: 'javascript:void(0);'},
+                                          css ({padding:'5px 0'}));
+        $(more_less_link).toggle (function () {$(this).text (settings.less_text);
+                                               // This is a hack to get the height of the div on line-height:'auto'.
+                                               box.inner.style.height = 'auto';
+                                               var box_height = $(box.inner).height ();
+                                               box.inner.style.height = height + 'px';
+                                               // We just had to do that because animate only works on numerical properties.
+                                               $(box.inner).animate ({height:box_height + 10 + 'px'}, 'slow');},
+                                  function () {$(this).text (settings.more_text);
+                                               $(box.inner).animate ({height:height + 'px'});});
 
-        var teaser_box = create ('div', css ({
-          backgroundColor:'#def',
-          font: '15px "Trebuchet MS", sans-serif',
-          margin: '20px 0',
-          padding: '10px',
-          border: '2px dashed black'
-        }));
+        var box = create ('div', css ({backgroundColor: '#def', 
+                                       border: '2px dashed black',
+                                       margin: '50px 0 20px 0',
+                                       padding: '10px',
+                                       width: '60%',
+                                       font: '12px "Trebuchet MS", sans-serif'}),
+                                 function () {this.inner = this.create ('div', text, css ({backgroundColor: '#def',
+                                                                                           font: '15px/' + settings.line_height_px + 'px "Trebuchet MS", sans-serif',
+                                                                                           height: height + 'px',
+                                                                                           overflow: 'hidden'}));},
+                                 more_less_link);
 
-        $('body').prepend ($(teaser_box).append (teaser, rest, $more_less_link));
+        $('body').prepend (box);
       });
     }
 
   });
+
+  $.fn.truncate.defaults = {
+    lines_visible  : 3,
+    line_height_px : 16,
+    more_text      : 'more',
+    less_text      : 'less'
+  };
+
 }) (jQuery);
